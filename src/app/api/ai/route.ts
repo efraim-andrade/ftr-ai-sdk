33dccc17a7245aac4fa92589c58dea29644b0c4e
Efraim Andrade <efraim.dev@gmail.com>
2025-04-29 23:37:34 -0300

    add the project components and integrate with the api.
    - Uses the tools to get api data to ai
    - Stream the messages to frontend

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,20 +1,53 @@
 import { openrouter } from "@/ai/open-router";
-import { generateObject } from "ai";
-import { type NextRequest, NextResponse } from "next/server";
+import { streamText, tool } from "ai";
+import type { NextRequest } from "next/server";
 import { z } from "zod";
 
-export async function GET(request: NextRequest) {
-	const result = await generateObject({
-		schema: z.object({
-			en: z.string().describe("Tradução para inglês"),
-			fr: z.string().describe("Tradução para francês"),
-			es: z.string().describe("Tradução para espanhol"),
-		}),
+export async function POST(request: NextRequest) {
+	const { messages } = await request.json();
+
+	const result = streamText({
 		model: openrouter.chat("openai/gpt-4o"),
-		prompt: 'Traduza "hello world" para diferentes idiomas!',
+		tools: {
+			profileAndUrls: tool({
+				description:
+					"Essa ferramenta serve para buscar dados do perfil de um usuário no Github ou acessar URLs da API para outras informações do usuário como lista de organizações, repositórios, eventos, seguidores, seguindo, etc...",
+				parameters: z.object({
+					username: z.string().describe("Username do usuário do Github"),
+				}),
+				execute: async ({ username }) => {
+					const response = await fetch(
+						`https://api.github.com/users/${username}`,
+					);
+					const data = await response.json();
+
+					return JSON.stringify(data);
+				},
+			}),
+
+			fetchHTTP: tool({
+				description:
+					"Essa ferramenta serve para realizar um requisição HTTP, em uma URL especificada e acessar sua resposta",
+				parameters: z.object({
+					url: z.string().url().describe("URL a ser requisitada"),
+				}),
+				execute: async ({ url }) => {
+					const response = await fetch(url);
+					const data = await response.json();
+
+					return JSON.stringify(data);
+				},
+			}),
+		},
+		messages,
+		maxSteps: 5,
 		system:
-			"Você é uma AI especializada em tradução, sempre retorne de maneira mais sucinta possível",
+			"Sempre responda em markdown sem aspas no início ou fim da mensagem.",
+
+		onStepFinish: ({ toolResults }) => {
+			console.log("tool results:", toolResults);
+		},
 	});
 
-	return NextResponse.json({ message: result.object });
+	return result.toDataStreamResponse();
 }
---
+++ src/components/chat.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useChat } from "@ai-sdk/react";
+import { Bot, User2 } from "lucide-react";
+import { useEffect, useRef } from "react";
+import { ChatScrollToBottomButton } from "./chat-scroll-to-bottom-button";
+import { Markdown } from "./markdown";
+import { MessageInput } from "./message-input";
+
+export function Chat() {
+	const { messages, input, handleInputChange, handleSubmit, status } = useChat({
+		api: "/api/ai",
+	});
+
+	const containerRef = useRef<HTMLDivElement>(null);
+	const bottomRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (containerRef.current) {
+			containerRef.current.scrollTo({
+				top: containerRef.current.scrollHeight,
+				behavior: "instant",
+			});
+		}
+	}, []);
+
+	// useEffect(() => {
+	//   if (messages.length > 0 && status === 'streaming' && containerRef.current) {
+	//     containerRef.current.scrollTo({
+	//       top: containerRef.current.scrollHeight,
+	//       behavior: "smooth",
+	//     })
+	//   }
+	// }, [
+	//   messages,
+	//   status,
+	// ])
+
+	return (
+		<>
+			<div className="flex-1 relative">
+				<div
+					ref={containerRef}
+					className="space-y-6 absolute inset-0 overflow-y-scroll scrollbar scrollbar-thumb-rounded-full scrollbar-thumb-zinc-900 scrollbar-track-transparent"
+				>
+					{messages.map((message) => {
+						return (
+							<div className="flex items-start gap-3" key={message.id}>
+								{message.role === "user" && (
+									<div className="size-7 rounded-md bg-zinc-900 flex items-center justify-center">
+										<User2 className="size-4 text-zinc-100" />
+									</div>
+								)}
+								{message.role === "assistant" && (
+									<div className="size-7 rounded-md bg-zinc-900 flex items-center justify-center">
+										<Bot className="size-4 text-zinc-400" />
+									</div>
+								)}
+
+								<div className="flex flex-col gap-4">
+									<div className="flex-1 prose prose-invert prose-zinc prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-h4:text-base prose-h5:text-sm prose-h6:text-xs">
+										<Markdown>{message.content}</Markdown>
+									</div>
+								</div>
+							</div>
+						);
+					})}
+
+					<div ref={bottomRef} />
+				</div>
+
+				<ChatScrollToBottomButton
+					containerRef={containerRef}
+					scrollRef={bottomRef}
+				/>
+			</div>
+
+			<MessageInput
+				disabled={status === "streaming" || status === "submitted"}
+				value={input}
+				onValueChange={handleInputChange}
+				onSubmit={handleSubmit}
+			/>
+		</>
+	);
+}
